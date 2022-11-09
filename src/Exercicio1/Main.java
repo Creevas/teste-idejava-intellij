9a674e05b70c6a40fed1fe182b41374f61b34150
Creevas <arcano.ph@gmail.com>
2022-11-09 16:06:34 -0300

    Criação calculadora

---
+++ src/Exercicio1/Main.java
@@ -0,0 +1,22 @@
+package Exercicio1;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("Exercício Calculadora");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
+
+        System.out.println("Exercício mensagem");
+        Mensagem.obterMensagem( 9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
+
+
+    }
+
+    }
+

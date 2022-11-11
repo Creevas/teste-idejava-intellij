05b5738c46ceebf56169eca4ebb6385b1b5f2902
Creevas <arcano.ph@gmail.com>
2022-11-11 10:37:22 -0300

    Criação calculadora

--- src/Sobrecarga/Main.java
+++ src/Sobrecarga/Main.java
@@ -5,8 +5,19 @@ public class Main {
     public static void main (String[] args){
 
         System.out.println("Exercicio quadriláteros");
-        Quadrilatero.area(3);
-        Quadrilatero.area(5,5);
-        Quadrilatero.area(7, 8, 9);
+
+        double areaQ = Quadrilatero.area(3);
+        System.out.println("Área do quadrado: " + areaQ);
+
+
+        double areaR = Quadrilatero.area(5,5);
+        System.out.println("Área do retângulo: " + areaR);
+
+
+        double areaT = Quadrilatero.area(7, 8, 9);
+        System.out.println("Área do trapézio: " + areaT);
+
+
+
     }
 }
--- src/Sobrecarga/Quadrilatero.java
+++ src/Sobrecarga/Quadrilatero.java
@@ -2,26 +2,26 @@ package Sobrecarga;
 
 public class Quadrilatero {
 
-    public static void area(double lado) {
+    public static double area(double lado) {
 
         double areaQ = lado * lado;
 
-        System.out.println("Área do quadrilatéro é " + areaQ);
+        return areaQ ;
     }
 
-    public static void area(double lado1, double lado2) {
+    public static double area(double lado1, double lado2) {
 
         double areaR = lado1 * lado2;
 
-        System.out.println("Área do retângulo é " + areaR);
+        return areaR;
 
     }
 
-    public static void area(double baseMaior, double baseMenor, double altura) {
+    public static double area(double baseMaior, double baseMenor, double altura) {
 
         double areaT =  ((baseMaior + baseMenor)*altura) / 2;
 
-        System.out.println("Área do Trapézio é " + areaT);
+        return areaT;
 
 
     }
